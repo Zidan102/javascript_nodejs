3fdba4a1e9cabdd0b7a8198bd11bc5724db67498
Zidan102 <yogumetokisaki@gmail.com>
2025-09-22 19:03:19 +0700

    Tugas Sesi 1

---
+++ sesi 1/test.js
@@ -0,0 +1,17 @@
+// Deklarasi variabel
+let matematika = 85;
+let bahasaInggris = 90;
+let ipa = 78;
+
+// Perhitungan rata-rata
+let nilaiRataRata = (matematika + bahasaInggris + ipa) / 3;
+
+// Tampilkan nilai rata-rata
+console.log("Nilai Rata-Rata:", nilaiRataRata);
+
+// Struktur kondisi if-else
+if (nilaiRataRata >= 80) {
+    console.log("Status: Lulus");
+} else {
+    console.log("Status: Tidak Lulus");
+}
